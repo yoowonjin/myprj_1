51c7a3a5e0946b933a369efa9acdf5f645b44a4b
yoowonjin <saydnjswls@naver.com>
2023-03-13 15:14:58 +0900

    삭제기능 추가

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-
 const $btnAdd = document.getElementById('btnAdd');
 $btnAdd.addEventListener('click',()=>{
     console.log('클릭');
+});
+
+const $btnDel = document.getElementById('btnDel');
+$btnAdd.addEventListener('click',()=>{
+    console.log('삭제');
 });
